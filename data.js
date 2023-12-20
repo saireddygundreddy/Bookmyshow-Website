37f5d9828e3d657a6629e155ec262a15a0e90eed
saireddygundreddy <137412098+saireddygundreddy@users.noreply.github.com>
2023-12-20 16:42:48 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+let movies = [
+    {
+        image: 'https://raw.githubusercontent.com/vu3tpz/BookMyShow-Clone/f447f7c6cb10cef285f7c1ffb61853bfb24fa11f/img/banner/banner1.webp'
+    },
+    {
+        image: 'https://raw.githubusercontent.com/vu3tpz/BookMyShow-Clone/f447f7c6cb10cef285f7c1ffb61853bfb24fa11f/img/banner/banner2.webp'
+    },
+    {
+        image: 'https://raw.githubusercontent.com/vu3tpz/BookMyShow-Clone/f447f7c6cb10cef285f7c1ffb61853bfb24fa11f/img/banner/banner3.webp'
+    },
+    {
+        image: 'https://raw.githubusercontent.com/vu3tpz/BookMyShow-Clone/f447f7c6cb10cef285f7c1ffb61853bfb24fa11f/img/banner/banner4.webp'
+    }
+]
